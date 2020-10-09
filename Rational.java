cf3a0e6f0a419ea7af5c26d0eefa3124f62b28fc
wagahai-memo <wagahai.memo@gmail.com>
2020-10-09 21:11:05 +0900

    Create Rational.java

---
+++ Rational.java
@@ -0,0 +1,10 @@
+public class Rational {
+    private final int numerator;
+    private final int denominator;
+
+    // プライマリコンストラクタ
+    public Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+}
